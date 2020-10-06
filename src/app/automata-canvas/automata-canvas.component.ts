7d724b3899993a0037686a4aaadab6eb48582985
Mario Bisignani <bisignam@gmail.com>
2020-10-07 01:08:22 +0200

    Styling

--- src/app/automata-canvas/automata-canvas.component.ts
+++ src/app/automata-canvas/automata-canvas.component.ts
@@ -1,26 +1,27 @@
-import { Component, ViewChild, AfterViewInit, ElementRef } from "@angular/core";
-import { BriansBrain } from "../automata-rules/briansbrain";
-import { P5Service } from "../automata-engine/p5-service";
-import { Pixel } from "../automata-engine/pixel";
-import { Color } from "../automata-engine/color";
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core'
+import { BriansBrain } from '../automata-rules/briansbrain'
+import { P5Service } from '../automata-engine/p5-service'
+import { Pixel } from '../automata-engine/pixel'
+import { Color } from '../automata-engine/color'
 
 @Component({
-  selector: "app-automata-canvas",
-  templateUrl: "./automata-canvas.component.html",
-  styleUrls: ["./automata-canvas.component.scss"],
+  selector: 'app-automata-canvas',
+  templateUrl: './automata-canvas.component.html',
+  styleUrls: ['./automata-canvas.component.scss'],
 })
 export class AutomataCanvasComponent implements AfterViewInit {
-  private p5Service: P5Service;
+  private p5Service: P5Service
 
+  @ViewChild('automataCanvasContainer')
   automataCanvasContainer: ElementRef;
 
-  ngAfterViewInit(): void {
+  ngAfterViewInit() {
     this.p5Service = new P5Service(
       1,
       new Color(100, 0, 100, 255),
       this.automataCanvasContainer.nativeElement
-    );
-    this.p5Service.startAutomata(new BriansBrain(), this.oscillator(), 1);
+    )
+    this.p5Service.startAutomata(new BriansBrain(), this.oscillator(), 1)
   }
 
   oscillator(): Array<Pixel> {
@@ -29,6 +30,6 @@ export class AutomataCanvasComponent implements AfterViewInit {
       Pixel.XY(50, 51),
       Pixel.XY(50, 52),
       Pixel.XY(51, 50),
-    ];
+    ]
   }
 }
