c7b801b67915ea00df040cce05b06bd0abdd88c2
Mario Bisignani <bisignam@gmail.com>
2020-10-08 01:39:24 +0200

    Implemented cancel button

--- src/app/automata-engine/grid.ts
+++ src/app/automata-engine/grid.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars-experimental
 import * as p5 from "p5";
 import { CellularAutomaton } from "./cellularautomaton";
 import { Color } from "./color";
