23ba0147988d811292892a932a234f697a503fba
Mario Bisignani <bisignam@gmail.com>
2020-10-24 22:44:23 +0200

    [FIX] Automaton not properly setting cells 'off' when launched, use get backgroundColor instead of original color

--- src/app/automata-rules/gameoflife.ts
+++ src/app/automata-rules/gameoflife.ts
@@ -11,9 +11,9 @@ export class GameOfLife extends CellularAutomaton {
   public applyRule(x: number, y: number): Color {
     const numberOfActiveNeighbors = this.mooreNeighbors(x, y);
     if (this.isActive(x, y) && numberOfActiveNeighbors < 2) {
-      return this.grid.getPixels()[x][y].getOriginalColor();
+      return this.grid.backgroundColor;
     } else if (this.isActive(x, y) && numberOfActiveNeighbors > 3) {
-      return this.grid.getPixels()[x][y].getOriginalColor();
+      return this.grid.backgroundColor;
     } else if (!this.isActive(x, y) && numberOfActiveNeighbors == 3) {
       return this._activationColor;
     }
