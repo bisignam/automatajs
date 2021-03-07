666f72d0df5bc20d5f31c620119c46b5cd986390
Mario Bisignani <bisignam@gmail.com>
2021-03-07 13:19:59 +0100

    [ENH] Migrate linter and formatter from TSLint to ESLint+Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  overrides: [
+    {
+      files: ['*.ts'],
+      parserOptions: {
+        project: ['tsconfig.*?.json', 'e2e/tsconfig.json'],
+        createDefaultProgram: true,
+      },
+      extends: [
+        'plugin:@angular-eslint/recommended',
+        // AirBnB Styleguide rules
+        'airbnb-typescript/base',
+        // Settings for Prettier
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+      ],
+    },
+    {
+      files: ['*.component.html'],
+      extends: ['plugin:@angular-eslint/template/recommended'],
+      rules: {
+        'max-len': ['error', { code: 80 }],
+      },
+    },
+    {
+      files: ['*.component.ts'],
+      extends: ['plugin:@angular-eslint/template/process-inline-templates'],
+    },
+  ],
+};
